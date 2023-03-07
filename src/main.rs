effd8d99cf06ffcb6d911d88e98dd9f89d5e9364
Alex <alexchen.info@gmail.com>
2023-03-08 00:23:16 +0800

    Style: remove comments

--- src/main.rs
+++ src/main.rs
@@ -30,25 +30,14 @@ impl KeyboardShortcut {
 }
 
 fn main() {
-    // Turn on raw mode for the terminal
     let mut stdout = stdout().into_raw_mode().unwrap();
 
-    // Define the available commands
+    let keyboard_shortcuts = vec![KeyboardShortcut {
+        key: 'g',
+        description: "This is a custom command",
+        command: "git status",
+    }];
 
-    let keyboard_shortcuts = vec![
-        KeyboardShortcut {
-            key: 'c',
-            description: "This is a custom command",
-            command: "echo 'Custom command executed\r\n'",
-        },
-        KeyboardShortcut {
-            key: 'g',
-            description: "This is a custom command",
-            command: "git status",
-        },
-    ];
-
-    // Print the messages to the terminal
     write!(stdout, "Please select a command:\r\n").unwrap();
 
     for keyboard_shortcut in &keyboard_shortcuts {
@@ -62,25 +51,19 @@ fn main() {
 
     stdout.flush().unwrap();
 
-    // Read input from stdin
     let input = stdin().keys();
 
-    // Loop over each key that is pressed
     for key in input {
-        // Match the key against the available commands
         match key.unwrap() {
-            // If the key matches a command, execute it and break the loop
             Key::Char(k) if keyboard_shortcuts.iter().any(|c| c.key == k) => {
                 let keyboard_shortcut = keyboard_shortcuts.iter().find(|c| c.key == k).unwrap();
                 keyboard_shortcut.execute_command(&mut stdout);
                 break;
             }
-            // Output other characters
             Key::Char(c) => {
                 write!(stdout, "You pressed: {}\r\n", c).unwrap();
                 stdout.flush().unwrap();
             }
-            // If the key is anything else, ignore it
             _ => {}
         }
     }
