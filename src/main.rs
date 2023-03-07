c3eab9da807a00ed86a70cb3d55f930b320f069b
Alex <alexchen.info@gmail.com>
2023-03-07 23:30:43 +0800

    Fix: commands not echoing anything

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 use std::io::{stdin, stdout, Write};
+use std::process::Command;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-struct Command {
+struct CustomCommand {
     key: char,
     description: &'static str,
-    execute: fn(),
+    shell_command: &'static str,
+}
+
+impl CustomCommand {
+    fn execute_shell_command(&self, stdout: &mut impl Write) {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg(self.shell_command)
+            .status();
+
+        match output {
+            Ok(status) => {
+                if status.success() {
+                    write!(stdout, "Command executed successfully\r\n").unwrap();
+                } else {
+                    write!(stdout, "Command execution failed\r\n").unwrap();
+                }
+            }
+            Err(e) => {
+                write!(stdout, "Error executing command: {:?}\r\n", e).unwrap();
+            }
+        }
+    }
 }
 
 fn main() {
@@ -14,24 +37,20 @@ fn main() {
     let mut stdout = stdout().into_raw_mode().unwrap();
 
     // Define the available commands
-    let commands = vec![
-        Command {
-            key: 'f',
-            description: "Feat",
-            execute: || println!("Executing Feat command\r\n"),
-        },
-        Command {
-            key: 'x',
-            description: "Fix",
-            execute: || println!("Executing Fix command\r\n"),
-        },
-    ];
+
+    let commands = vec![CustomCommand {
+        key: 'c',
+        description: "This is a custom command",
+        shell_command: "echo 'Custom command executed\r\n'",
+    }];
 
     // Print the messages to the terminal
     write!(stdout, "Please select a command:\r\n").unwrap();
+
     for command in &commands {
         write!(stdout, "{}  {}\r\n", command.key, command.description).unwrap();
     }
+
     stdout.flush().unwrap();
 
     // Read input from stdin
@@ -44,10 +63,10 @@ fn main() {
             // If the key matches a command, execute it and break the loop
             Key::Char(k) if commands.iter().any(|c| c.key == k) => {
                 let command = commands.iter().find(|c| c.key == k).unwrap();
-                (command.execute)();
+                command.execute_shell_command(&mut stdout);
                 break;
             }
-            // If the key is any other printable character, print it to the screen
+            // Output other characters
             Key::Char(c) => {
                 write!(stdout, "You pressed: {}\r\n", c).unwrap();
                 stdout.flush().unwrap();
