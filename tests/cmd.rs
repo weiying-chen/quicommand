252dcb608925762a88fa4d536b1c0c5d35ff1d8d
Alex <alexchen.info@gmail.com>
2023-05-08 21:32:06 +0800

    Move `main()` tests to test file

--- tests/cmd.rs
+++
@@ -1,9 +0,0 @@
-// use assert_cmd::Command;
-
-// #[test]
-// fn test_run() -> Result<(), Box<dyn std::error::Error>> {
-//     let mut cmd = Command::cargo_bin("keymap")?;
-//     cmd.assert().success();
-
-//     Ok(())
-// }
