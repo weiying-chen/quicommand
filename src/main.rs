161f99e38db832fb89843beffd903c2da2b26f8a
Alex <alexchen.info@gmail.com>
2023-06-08 21:25:42 +0800

    Create `Screen` struct

--- src/main.rs
+++ src/main.rs
@@ -41,118 +41,120 @@ impl TermCursor for RawStdout {
     }
 }
 
-//To-do: maybe functions like these should belong to `TermWriter`?
-fn handle_quit(stdout: &mut RawStdout) {
-    stdout
-        .write_term(format_args!("{}", termion::cursor::Show))
-        .unwrap();
+struct Screen<T: TermCursor + Write> {
+    stdout: T,
 }
 
-fn prompt_input<T: TermCursor + Write>(message: &str, stdout: &mut T) {
-    stdout
-        .write_term(format_args!("{}{}", termion::cursor::Show, message))
-        .unwrap();
-    stdout.flush().unwrap();
-}
-
-fn handle_input_result<T: TermCursor + Write>(
-    result: Result<Input, InputError>,
-    keymap: &Keymap,
-    mut stdout: T,
-) {
-    match result {
-        Ok(Input::Text(i)) => {
-            // Because the input doesn't start a newline
-            stdout.write_term(format_args!("\r\n")).unwrap();
-
-            stdout
-                .write_term(format_args!("{}", termion::cursor::Show))
-                .unwrap();
-
-            stdout.flush().unwrap();
-
-            drop(stdout);
-
-            // To-do: `command should` return a result.
-            // To-do: The cursor is shown previously in prompt_input.
-            let mut command = CmdRunner::new(keymap.command, Some(&i));
-            // let stdout_mutex = Arc::new(Mutex::new(Some(stdout)));
-
-            command.run().unwrap();
-        }
-        Ok(Input::None) => {
-            stdout
-                .write_term(format_args!("{}", termion::cursor::Show))
-                .unwrap();
-
-            stdout.flush().unwrap();
-
-            drop(stdout);
-
-            let mut command = CmdRunner::new(keymap.command, None);
-            // let stdout_mutex = Arc::new(Mutex::new(Some(stdout)));
-
-            // command.run(stdout_mutex);
-
-            command.run().unwrap();
-            // println!("OUTPUT: {:?}", &output);
-        }
-        Ok(Input::Exit) => {
-            stdout.write_term(format_args!("\r\n")).unwrap();
-        }
-        Err(e) => {
-            stdout
-                .write_term(format_args!("Invalid input: {}\r\n", e))
-                .unwrap();
-        }
-    };
-}
-
-fn handle_input<T: TermCursor + Write>(
-    key: char,
-    keymaps: &[Keymap],
-    stdin: impl Iterator<Item = Result<Key, std::io::Error>>,
-    mut stdout: T,
-) {
-    if let Some(keymap) = keymaps.iter().find(|k| k.key == key) {
-        match keymap.prompt {
-            Some(_) => {
-                prompt_input(keymap.prompt.unwrap(), &mut stdout);
-
-                let input = keymap::input::get_input(stdin, &mut stdout);
-
-                handle_input_result(input, &keymap, stdout);
-            }
-            None => {
-                handle_input_result(Ok(Input::None), &keymap, stdout);
-            }
-        }
+impl<T: TermCursor + Write> Screen<T> {
+    //To-do: maybe functions like these should belong to `TermWriter`?
+    fn new(stdout: T) -> Self {
+        Screen { stdout }
     }
-}
 
-fn show_keymap_menu<T: TermCursor + Write>(keymaps: &[Keymap], stdout: &mut T) {
-    stdout
-        .write_term(format_args!(
-            "{}{}Please select a command:{}\r\n",
-            termion::clear::All,
-            termion::cursor::Goto(1, 1),
-            termion::cursor::Hide,
-        ))
-        .unwrap();
-
-    for keymap in keymaps {
-        stdout
-            .write_term(format_args!("{}  {}\r\n", keymap.key, keymap.description))
+    fn handle_quit(&mut self) {
+        self.stdout
+            .write_term(format_args!("{}", termion::cursor::Show))
             .unwrap();
     }
 
-    stdout.flush().unwrap();
+    fn prompt_input(&mut self, message: &str) {
+        self.stdout
+            .write_term(format_args!("{}{}", termion::cursor::Show, message))
+            .unwrap();
+        self.stdout.flush().unwrap();
+    }
+
+    fn handle_input_result(mut self, result: Result<Input, InputError>, keymap: &Keymap) {
+        match result {
+            Ok(Input::Text(i)) => {
+                // Because the input doesn't start a newline
+                self.stdout.write_term(format_args!("\r\n")).unwrap();
+
+                self.stdout
+                    .write_term(format_args!("{}", termion::cursor::Show))
+                    .unwrap();
+
+                self.stdout.flush().unwrap();
+
+                drop(self.stdout);
+
+                // To-do: `command should` return a result.
+                // To-do: The cursor is shown previously in prompt_input.
+                let mut command = CmdRunner::new(keymap.command, Some(&i));
+                // let stdout_mutex = Arc::new(Mutex::new(Some(stdout)));
+
+                command.run().unwrap();
+            }
+            Ok(Input::None) => {
+                self.stdout
+                    .write_term(format_args!("{}", termion::cursor::Show))
+                    .unwrap();
+
+                self.stdout.flush().unwrap();
+                drop(self.stdout);
+
+                let mut command = CmdRunner::new(keymap.command, None);
+
+                command.run().unwrap();
+            }
+            Ok(Input::Exit) => {
+                self.stdout.write_term(format_args!("\r\n")).unwrap();
+            }
+            Err(e) => {
+                self.stdout
+                    .write_term(format_args!("Invalid input: {}\r\n", e))
+                    .unwrap();
+            }
+        };
+    }
+
+    fn handle_input(
+        mut self,
+        key: char,
+        keymaps: &[Keymap],
+        stdin: impl Iterator<Item = Result<Key, std::io::Error>>,
+    ) {
+        if let Some(keymap) = keymaps.iter().find(|k| k.key == key) {
+            match keymap.prompt {
+                Some(_) => {
+                    self.prompt_input(keymap.prompt.unwrap());
+
+                    let input = keymap::input::get_input(stdin, &mut self.stdout);
+
+                    self.handle_input_result(input, &keymap);
+                }
+                None => {
+                    self.handle_input_result(Ok(Input::None), &keymap);
+                }
+            }
+        }
+    }
+
+    fn show_keymap_menu(&mut self, keymaps: &[Keymap]) {
+        self.stdout
+            .write_term(format_args!(
+                "{}{}Please select a command:{}\r\n",
+                termion::clear::All,
+                termion::cursor::Goto(1, 1),
+                termion::cursor::Hide,
+            ))
+            .unwrap();
+
+        for keymap in keymaps {
+            self.stdout
+                .write_term(format_args!("{}  {}\r\n", keymap.key, keymap.description))
+                .unwrap();
+        }
+
+        self.stdout.flush().unwrap();
+    }
 }
 
 fn main() {
-    let mut stdout = RawStdout::new().unwrap();
+    let stdout = RawStdout::new().unwrap();
+    let mut screen = Screen::new(stdout);
 
-    stdout.flush().unwrap();
+    screen.stdout.flush().unwrap();
 
     let keymaps = vec![
         Keymap::new('t', "Sleep", "sleep 2 && echo test && sleep 2"),
@@ -174,17 +176,17 @@ fn main() {
         ),
     ];
 
-    show_keymap_menu(&keymaps, &mut stdout);
-    stdout.flush().unwrap();
+    screen.show_keymap_menu(&keymaps);
+    screen.stdout.flush().unwrap();
 
     for key in stdin().keys() {
         match key.unwrap() {
             Key::Char('q') => {
-                handle_quit(&mut stdout);
+                screen.handle_quit();
                 break;
             }
             Key::Char(c) => {
-                handle_input(c, &keymaps, stdin().keys(), stdout);
+                screen.handle_input(c, &keymaps, stdin().keys());
                 break;
             }
             _ => {}
